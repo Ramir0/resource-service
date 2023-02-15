69ab339dbc6038356109b8f5b808528597c9d006
Amir Aranibar <amir.epam@tripactions.tech>
2023-02-15 17:41:29 +0100

    Change retry mechanism to exponential backoff

--- application/src/main/java/dev/amir/resourceservice/application/retry/configuration/RetryConfiguration.java
+++ application/src/main/java/dev/amir/resourceservice/application/retry/configuration/RetryConfiguration.java
@@ -1,6 +1,5 @@
 package dev.amir.resourceservice.application.retry.configuration;
 
-import dev.amir.resourceservice.domain.exception.UnexpectedResourceException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,18 +10,24 @@ import org.springframework.retry.support.RetryTemplate;
 @Configuration
 public class RetryConfiguration {
 
-    @Value("${spring.retry.maxAttempts}")
+    @Value("${spring.retry.max-attempts}")
     private Integer maxAttempts;
 
-    @Value("${spring.retry.maxDelayInMillis}")
-    private Long maxDelayInMillis;
+    @Value("${spring.retry.initial-interval}")
+    private Long initialInterval;
+
+    @Value("${spring.retry.multiplier}")
+    private Long multiplier;
+
+    @Value("${spring.retry.max-interval}")
+    private Long maxInterval;
 
     @Bean
     public RetryTemplate retryTemplate() {
         return RetryTemplate.builder()
                 .maxAttempts(maxAttempts)
-                .fixedBackoff(maxDelayInMillis)
-                .retryOn(UnexpectedResourceException.class)
+                .exponentialBackoff(initialInterval, multiplier, maxInterval)
+                .retryOn(Exception.class)
                 .build();
     }
 }
